43ec1b09712f799f5e4da34697f5821c47cc79be
andrea <eandram@hotmail.it>
2017-08-02 12:22:26 +0100

    api Json service started

---
+++ api.php
@@ -0,0 +1,7 @@
+<?php
+// Require necessary files
+require 'includes/config.php';
+include  'includes/functions.php';
+
+//instantiate the myDb class and GET the views to display
+require  'includes/dbConnect.php';
--- json.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: desktop
- * Date: 7/17/2017
- * Time: 7:56 PM
- */
